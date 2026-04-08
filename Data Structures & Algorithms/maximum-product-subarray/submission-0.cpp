4021953d4996bfdbd91eded92fac566f5c920565
Joon <junxianchen2005@gmail.com>
2026-04-08 17:29:51 +1000

    Add: maximum-product-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        std::vector<int> dp(n + 1, 0);
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++){
+            dp[i] = std::max(nums[i], dp[i-1] * nums[i]);
+        }
+        return dp[n-1];
+    }
+};
