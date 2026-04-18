fa6f561f96cf9cb2944454c5185461bc14b81ec9
Joon <junxianchen2005@gmail.com>
2026-04-18 16:26:54 +1000

    Add: baseball-game - submission-3

---
+++ Data Structures & Algorithms/baseball-game/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        std::stack<int> board;
+        int maxSum = 0;
+        for (const std::string& op : operations){
+            if (op == "+"){
+                int nums1 = board.top();
+                board.pop();
+                int nums2 = board.top();
+                int sum = nums1 + nums2;
+                board.push(nums1);
+                board.push(sum);
+                maxSum += board.top();
+            } else if (op == "D"){
+                int nums1 = board.top() * 2;
+                board.push(nums1);
+                maxSum += board.top();
+            } else if (op == "C") {
+                maxSum -= board.top();
+                board.pop();
+            } else {    
+                int value = std::stoi(op);
+                board.push(value);
+                maxSum += board.top();
+            }
+        }
+        return maxSum;
+    }
+};
