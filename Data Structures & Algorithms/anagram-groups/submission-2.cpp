d854b5a59c78b305c75288b62896a675f7a462fe
Joon <junxianchen2005@gmail.com>
2026-04-25 23:38:54 +1000

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        std::vector<std::vector<std::string>> results;
+        std::unordered_map<std::string, std::vector<std::string>> hashMap;
+
+        for (const std::string& str : strs){
+            std::vector<int> arrays(26);
+            for (const char c : str){
+                arrays[c - 'a'] = c;
+            }
+            std::string temp = "";
+            for (int i = 0; i < 26; i++){
+                temp += std::to_string(arrays[i]) + '#';
+            }
+            hashMap[temp].push_back(str);
+        }
+        for (const auto& it : hashMap){
+            results.push_back(it.second);
+        }
+        return results;
+    }
+};
