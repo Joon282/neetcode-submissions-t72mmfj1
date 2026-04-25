791dafc03539e5bc94a383f801a0afd67f873127
Joon <junxianchen2005@gmail.com>
2026-04-25 21:00:18 +1000

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::unordered_map<char, int> freq;
+        std::unordered_map<char, int> freq2;
+        for (char c : s){
+            freq[c]++;
+        }
+        for (char c : t){
+            freq2[c]++;
+        }
+        return freq == freq2;
+    }
+};
