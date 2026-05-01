2d42984127c56671b729f3df243111b0a984c68e
Joon <junxianchen2005@gmail.com>
2026-05-01 19:55:06 +1000

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void merge (std::vector<int>& array, int left, int mid ,int right){
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        std::vector<int> vec1;
+        std::vector<int> vec2;
+        for (int i = 0; i < n1; i++){
+            vec1.push_back(array[left + i]);
+        }
+        for (int i = 0; i < n2; i++){
+            vec2.push_back(array[mid + 1 + i]);
+        }
+        int i = 0;
+        int j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (vec1[i] < vec2[j]){
+                array[k++] = vec1[i++];
+            } else {
+                array[k++] = vec2[j++];
+            }
+        }
+        while (j < n2){
+            array[k++] = vec2[j++];
+        }
+        while (i < n1){
+            array[k++] = vec1[i++];
+        }
+    }
+
+    void mergeSort(std::vector<int>&nums, int left ,int right){
+        if (left >= right){
+            return;
+        }
+        int mid = left + (right - left)/2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid+1, right);
+        merge(nums,left, mid, right);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size()-1;
+        mergeSort(nums, left ,right);
+        return nums;
+    }
+};
