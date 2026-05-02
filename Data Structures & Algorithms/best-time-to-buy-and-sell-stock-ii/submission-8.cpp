88bdb7079bfb0d2781d68d494a012d3a4d949a6a
Joon <junxianchen2005@gmail.com>
2026-05-02 16:17:34 +1000

    Add: best-time-to-buy-and-sell-stock-ii - submission-8

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-8.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max = 0;
+        int j = 1;
+        for (int i = 0; i < prices.size(); i++){
+            if (prices[i] >= prices[j]){
+                j++;
+                continue;
+            } else {
+                int profit = prices[j] - prices[i];
+                max += profit;
+                j++;
+            }
+        }   
+        return max;
+    }
+};
