c16099c591cc46757c7d1748b72b584887237cb5
Joon <junxianchen2005@gmail.com>
2026-09-09 19:55:11 +1000

    Add: 4sum - submission-11

---
+++ Data Structures & Algorithms/4sum/submission-11.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        if (nums.size() < 4){
+            return {};
+        }
+        std::sort(nums.begin(), nums.end());
+        std::vector<std::vector<int>> results;
+        // [-3,0,1,2,3,3]
+        for (int i = 0; i < nums.size()-3; i++){
+            if (i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            for (int j = i+1; j < nums.size()-2; j++){
+                if (j > i+1 && nums[j] == nums[j-1]){
+                    continue;
+                }
+                int k = j+1;
+                int l = nums.size()-1;
+                long long difference = (long long) target - nums[i] - nums[j];
+                while (k < l){
+                    long long sum = (long long) nums[k] + nums[l];
+                    if (sum == difference){
+                        results.push_back({nums[i],nums[j],nums[k],nums[l]});
+                        while (k < l && (nums[k] == nums[k+1])) {
+                            k++;
+                        }
+                        while (k < l && (nums[l] == nums[l-1])) {
+                            l--;
+                        }
+                        k++;
+                        l--;
+                    } else if (sum > difference){
+                        l--;
+                    } else {
+                        k++;
+                    }
+                }
+            }
+        }
+        return results;
+    }
+};
