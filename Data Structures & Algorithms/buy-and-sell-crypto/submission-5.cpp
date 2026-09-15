9b59eb83b1b40e79693c33477be315bb671fb36f
Joon <junxianchen2005@gmail.com>
2026-09-15 16:13:40 +1000

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max = 0;
+        int left = 0;
+        int right = 1;
+        while (right < prices.size()){
+            if (prices[right] > prices[left]){
+                int profit = prices[right] - prices[left];
+                max = std::max(profit,max);
+            } else {
+                left = right;
+            }
+            right++;
+        }
+        return max;
+    }
+    
+};
